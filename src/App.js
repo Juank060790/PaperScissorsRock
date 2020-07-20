0dd15690a497f6fdca2677225fd9762cae94e22e
Juank060790 <juank060790@gmail.com>
2020-07-20 15:03:00 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import React, { Component } from 'react';
+import './App.css';
+import ChoiceCard from './components/Box'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const choices = {
+  rock:
+    "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
+  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
+  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
+};
+
+function App() {
+  return (
+    <div className="App">
+   <ChoiceCard
+   title="You" 
+   winner={false}
+   imgURL={choices.rock}
+   />
+
+<ChoiceCard
+   title="Computer" 
+   winner={true} 
+   imgURL={choices.paper}
+   
+   />
+
+  </div>
+  );
+}
+
+export default App;

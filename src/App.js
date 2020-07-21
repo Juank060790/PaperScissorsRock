d4d5cd6fecbf663651f7ecd54b6c712188cebc8f
Juank060790 <juank060790@gmail.com>
2020-07-21 10:01:09 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import ChoiceCard from './components/Box'
 import 'bootstrap/dist/css/bootstrap.min.css';
